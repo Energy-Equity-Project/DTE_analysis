73169553644c98e3b8236beeb60d5fb8591acb4f
Rahul Agrawal Bejarano <rahul.agrawal.b@gmail.com>
2024-04-21 17:53:06 -0400

    generalizable code to convert section data from pdf to CSV

--- clean_quarterly_dte_reports.R
+++ clean_quarterly_dte_reports.R
@@ -5,82 +5,63 @@ library(tidyverse)
 library(pdftools)
 
 datadir <- "../Data/DTE_Quarterly_Reporting_MPSC"
-outdir <- "outputs/DTE_quarter_reports_disconnections"
+outdir <- "outputs/DTE_quarter_reports"
 quarter_reports <- sort(list.files(file.path(datadir)))
 
-pdf_relevant_data <- function(fp) {
-  # Read in pdf file as text
-  data <- pdf_text(fp)
+get_section_lines <- function(data, section_regexpr, num_lines_to_keep = 2) {
   # remove all commas (ie 1,500) and separate by new line character
   lines <-  gsub(",", "", data) %>%
     read_lines()
   
+  section_lines <- c()
   # Keep relevant lines
-  relevant_lines <- c()
   for (i in 1:length(lines)) {
-    # Only keep table section headers and lines with data in them
-    section_headers_regexpr <- "Customer Service|Customer Payment Performance|Winter Protection Plan \\(WPP\\)|Alternative Shutoff Protection Plan|Informal Hearings|Shutoff Information|Restoration Information"
-    data_regexpr <- "(\\s+\\d+\\s+|\\s+\\d+\\s+\\d+\\s+|\\s+\\d+\\s+\\d+\\s+\\d+\\s+|\\s+\\d+\\s+\\d+\\s+\\d+\\s+\\d+\\s+)"
-    keep_line_pattern = paste(section_headers_regexpr, "|", data_regexpr, sep = "")
-    if (str_detect(lines[i], pattern = keep_line_pattern)) {
-      
-      # if line is a section header then remove all leading white space
-      if (str_detect(lines[i], section_headers_regexpr)) {
-        section_header <- str_extract(lines[i], section_headers_regexpr)
-        relevant_lines <- c(relevant_lines, section_header)
-      } else {
-        relevant_lines <- c(relevant_lines, lines[i])
+    if (str_detect(lines[i], section_regexpr)) {
+      j <- 1
+      lines_added <- 1
+      data_regexpr <- "\\s+\\d+\\s+|\\s+\\d+\\s+\\d+\\s+|\\s+\\d+\\s+\\d+\\s+\\d+\\s+|\\s+\\d+\\s+\\d+\\s+\\d+\\s+\\d+\\s+"
+      while (lines_added <= num_lines_to_keep) {
+        if (str_detect(lines[i+j], data_regexpr)) {
+          section_lines <- c(section_lines, lines[i+j])
+          lines_added <- lines_added + 1
+        }
+        
+        j <- j + 1
       }
       
+      break
     }
   }
   
-  return(relevant_lines)
+  return(section_lines)
 }
 
-get_shutoff_lines <- function(fp) {
-  # Read in pdf file as text
-  data <- pdf_text(fp)
-  # remove all commas (ie 1,500) and separate by new line character
-  lines <-  gsub(",", "", data) %>%
-    read_lines()
-  
-  # Keep relevant lines
-  shutoffs_line_regexpr <- "Total of customers physically discontinued due to non-payment"
-  for (i in 1:length(lines)) {
-    if (str_detect(lines[i], shutoffs_line_regexpr)) {
-      return(lines[(i+1):(i+2)])
-    }
-  }
-  return(NULL)
-}
-
-get_shutoff_data <- function(lines, year, quarter) {
+format_data <- function(lines, year, quarter, item_title, items, val_header) {
   
   if (quarter == 1) {
     headers <- c(
-      "utility",
+      "item_col",
       paste("01-01-", year, sep = ""),
       paste("01-02-", year, sep = ""),
       paste("01-03-", year, sep = "")
     )
   } else if (quarter == 2) {
     headers <- c(
-      "utility",
+      "item_col",
       paste("01-04-", year, sep = ""),
       paste("01-05-", year, sep = ""),
       paste("01-06-", year, sep = "")
     )
   } else if (quarter == 3) {
     headers <- c(
-      "utility",
+      "item_col",
       paste("01-07-", year, sep = ""),
       paste("01-08-", year, sep = ""),
       paste("01-09-", year, sep = "")
     )
   } else if (quarter == 4) {
     headers <- c(
-      "utility",
+      "item_col",
       paste("01-10-", year, sep = ""),
       paste("01-11-", year, sep = ""),
       paste("01-12-", year, sep = "")
@@ -89,48 +70,71 @@ get_shutoff_data <- function(lines, year, quarter) {
     return(data.frame())
   }
   
-  electric_shutoffs <- unlist(str_extract_all(lines[1], "\\d+"))
-  gas_shutoffs <- unlist(str_extract_all(lines[2], "\\d+"))
+  num_items <- length(items)
   
-  shutoffs_df <- data.frame(
-    utility = c("Electric", "Natural Gas"),
-    a = c(electric_shutoffs[1], gas_shutoffs[1]),
-    b = c(electric_shutoffs[2], gas_shutoffs[2]),
-    c = c(electric_shutoffs[3], gas_shutoffs[3])
-  )
+  df <- data.frame()
   
-  colnames(shutoffs_df) <- headers
-  shutoffs_df <- shutoffs_df %>%
-    pivot_longer(-c(utility), names_to = "date", values_to = "disconnections")
+  for (i in 1:num_items) {
+    
+    curr_item_data <- unlist(str_extract_all(lines[i], "\\d+"))
+    df <- df %>%
+      bind_rows(data.frame(
+        item = items[i],
+        a = curr_item_data[1],
+        b = curr_item_data[2],
+        c = curr_item_data[3]
+      ))
+  }
+  
+  colnames(df) <- headers
+  df <- df %>%
+    pivot_longer(-c(item_col), names_to = "date", values_to = val_header)
+  
+  colnames(df) <- c(item_title, "date", val_header)
   
   
-  return(shutoffs_df)
+  return(df)
 }
-
 
 for (i in 1:length(quarter_reports)) {
   print(paste("processing", quarter_reports[i]))
   curr_year <- substr(quarter_reports[i], 1, 4)
   curr_quarter <- substr(quarter_reports[i], 6, 6)
-  # relevant_lines <- pdf_relevant_data(file.path(datadir, quarter_reports[i]))
-  shutoff_lines <- get_shutoff_lines(file.path(datadir, quarter_reports[i]))
+
+  # Read in pdf file as text
+  data <- pdf_text(file.path(datadir, quarter_reports[i]))
   
-  # shutoffs_line_regexpr <- "Total of customers physically discontinued due to non-payment"
-  # shutoffs_line_idx <- 0
-  # for (j in 1:length(relevant_lines)) {
-  #   if (str_detect(relevant_lines[j], shutoffs_line_regexpr)) {
-  #     shutoffs_line_idx <- j
-  #     break
-  #   }
-  # }
+  # Collecting disconnections due to non-payment from PDF to CSV
+  shutoff_lines <- get_section_lines(data, "Total of customers physically discontinued due to non-payment", 2)
+  shutoffs <- format_data(shutoff_lines, curr_year, curr_quarter,
+                          "utility", c("Electric", "Natural Gas"), "disconnections")
+  disconnections_outfile <- paste("disconnections_", curr_year, "_q", curr_quarter, ".csv", sep = "")
+  write.csv(shutoffs, file.path(outdir, disconnections_outfile), row.names = FALSE)
   
-  # shutoff_lines <- relevant_lines[(shutoffs_line_idx + 1):(shutoffs_line_idx + 2)]
-  print(shutoff_lines)
-  shutoffs <- get_shutoff_data(shutoff_lines, curr_year, curr_quarter)
-  out_filename <- paste("disconnections_", curr_year, "_q", curr_quarter, ".csv", sep = "")
-  write.csv(shutoffs, file.path(outdir, out_filename), row.names = FALSE)
+  # Collecting total number of customers restored
+  restoration_lines <- get_section_lines(data, "Total number of customers restored", 2)
+  restorations <- format_data(restoration_lines, curr_year, curr_quarter,
+                              "utility", c("Electric", "Natural Gas"), "restorations")
+  restorations_outfile <- paste("restorations_", curr_year, "_q", curr_quarter, ".csv", sep = "")
+  write.csv(restorations, file.path(outdir, restorations_outfile), row.names = FALSE)
   
-  print(paste("writing", out_filename))
+  # Collecting total number of enrollments in alternative shutoff protection plan
+  # Note this has to include total enrollments to calculate number of non low income non seniors
+  alt_shutoff_lines <- get_section_lines(data, "Alternative Shutoff Protection Plan", 3)
+  alt_shutoff_enrollments <- format_data(alt_shutoff_lines, curr_year, curr_quarter,
+                                         "customer_grp", c("total", "low_income", "seniors"), "enrollments")
+  alt_shutoff_outfile <- paste("alternative_shutoff_protection_plan_", curr_year, "_q", curr_quarter, ".csv", sep = "")
+  write.csv(alt_shutoff_enrollments, file.path(outdir, alt_shutoff_outfile), row.names = FALSE)
+  
+  # Collecting total number of enrollments in alternative shutoff protection plan
+  # Note this has to include total enrollments to calculate number of non low income non seniors
+  wpp_lines <- get_section_lines(data, "Winter Protection Plan \\(WPP\\)", 3)
+  wpp_enrollments <- format_data(wpp_lines, curr_year, curr_quarter,
+                                         "customer_grp", c("total", "low_income", "seniors"), "enrollments")
+  wpp_outfile <- paste("winter_protection_plan_", curr_year, "_q", curr_quarter, ".csv", sep = "")
+  write.csv(wpp_enrollments, file.path(outdir, wpp_outfile), row.names = FALSE)
+  
+  print(paste("writing", curr_year, curr_quarter))
 }
 
 print("DONE")
